b6c64df39ea9db5b5581d70f2b625592e5214ba1
Mark Jubenville <ioncache@gmail.com>
2012-12-10 11:42:51 -0500

    added a db

--- lib/GameMatcher.pm
+++ lib/GameMatcher.pm
@@ -1,5 +1,7 @@
 package GameMatcher;
+
 use Dancer ':syntax';
+#use Dancer::Plugin::Resource;
 
 get '/' => sub {
     template 'index';
--- test.pl
+++ test.pl
@@ -6,7 +6,22 @@ use Data::Printer;
 
 use GameMatcher::Schema;
 
-my $schema = GameMatcher::Schema->new();
+my $schema = GameMatcher::Schema->connect(
+    'dbi:SQLite:dbname=game_matcher.sqlite',
+    '', '', 
+    { RaiseError => 1, AutoCommit => 1 }
+);
 
-p $schema;
+#$schema->deploy;
+
+my $players = $schema->resultset('Players');
+
+my $new_player = $players->create(
+    {   email    => 'markj@oanda.com',
+        name     => 'Mark Jubenville',
+        nickname => 'ioncache',
+    }
+);
+
+p $new_player;
 
