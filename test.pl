3208daa10cc2d32c954578d50263d53c81d694d5
Mark Jubenville <ioncache@gmail.com>
2012-12-10 15:54:26 -0500

    updated db and test

--- test.pl
+++ test.pl
@@ -7,21 +7,32 @@ use Data::Printer;
 use GameMatcher::Schema;
 
 my $schema = GameMatcher::Schema->connect(
-    'dbi:SQLite:dbname=game_matcher.sqlite',
-    '', '', 
-    { RaiseError => 1, AutoCommit => 1 }
+    'dbi:SQLite:dbname=db/game_matcher.sqlite',
+    '',
+    '', 
+    {
+        AutoCommit => 1,
+        RaiseError => 1,
+        on_connect_call => 'use_foreign_keys',
+    }
 );
 
 #$schema->deploy;
+#
+#say "done deploy";
 
-my $players = $schema->resultset('Players');
+my $players = $schema->resultset('GameMatcher::Schema::Result::Players');
+
+say "done resultset";
 
 my $new_player = $players->create(
     {   email    => 'markj@oanda.com',
         name     => 'Mark Jubenville',
-        nickname => 'ioncache',
+        nickname => 'ioncache_' . time,
     }
 );
 
-p $new_player;
+say "done creating player";
+
+say $new_player->name;
 
